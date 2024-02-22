720071267e357eefe61df19be647401953fd8890
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-02-22 21:13:06 +0000

    third commit

--- src/component/TrafficLight.jsx
+++ src/component/TrafficLight.jsx
@@ -3,8 +3,18 @@ import React, { useState } from "react";
 const TrafficLight = () => {
   const [color, setColor] = useState("");
 
+  const handleClick = () => {
+    const colors = ["red", "yellow", "green"];
+    const currentIndex = colors.indexOf(color);
+    console.log("currentIndex", currentIndex);
+
+    const nextIndex = (currentIndex + 1) % colors.length;
+    console.log("nextIndex", nextIndex);
+    setColor(colors[nextIndex]);
+  };
+
   return (
-    <div className="container h-100 d-flex justify-content-center align-items-center ">
+    <div className="container h-100 d-flex flex-column justify-content-center align-items-center ">
       <div className="trafficLight">
         <div
           className={`red ${color === "red" ? "selected" : ""}`}
@@ -21,6 +31,14 @@ const TrafficLight = () => {
           onClick={() => setColor((prev) => (prev = "green"))}
         ></div>
       </div>
+
+      <button
+        type="button"
+        className="btn btn-dark btn-lg m-3"
+        onClick={handleClick}
+      >
+        Click me!
+      </button>
     </div>
   );
 };
